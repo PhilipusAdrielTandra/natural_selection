8b24a8735cb61adcf7c185cc7703d3a76d499c42
daffasyaidina <91464359+daffasyaidina@users.noreply.github.com>
2023-11-20 22:27:10 +0700

    Added random movement

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 import pygame
 import math
 import random
-import sys
-import os
 from moth import Moth
 from food import Food
-import neat
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 food = Food()
-moth = Moth()
+moths = [Moth()]
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            moths.append(Moth())
 
     screen.fill("white")
     food.draw(screen)
-    moth.draw(screen)
-    moth.move()
-    pygame.display.flip()
 
-    clock.tick(60) 
+    for i, moth1 in enumerate(moths):
+        moth1.draw(screen)
+        moth1.move()
+
+        if math.hypot(moth1.x - food.x, moth1.y - food.y) < moth1.radius + food.size[0]/2:
+            food.eaten = True
+
+        for j, moth2 in enumerate(moths):
+            if i != j:
+                if math.hypot(moth1.x - moth2.x, moth1.y - moth2.y) < moth1.radius + moth2.radius:
+                    pass
+
+    if food.eaten:
+        food = Food()
+
+    pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
--- moth.py
+++ moth.py
@@ -6,34 +6,18 @@ class Moth:
     def __init__(self):
         self.x = random.randint(1, 1280)
         self.y = random.randint(1, 720)
-        self.angle = 0
-        self.velocity = 5
+        self.angle = random.uniform(0, 2 * math.pi)
+        self.velocity = 3
         self.radius = 8
 
     def draw(self, win):
         pygame.draw.circle(win, (0, 0, 0), (self.x, self.y), self.radius)
 
-    #input for testing
     def move(self):
-        sin_a = math.sin(self.angle)
-        cos_a = math.cos(self.angle)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.x += self.velocity * cos_a
-            self.y += self.velocity * sin_a
-        if keys[pygame.K_d]:
-            self.x += -self.velocity * cos_a
-            self.y += -self.velocity * sin_a
-        if keys[pygame.K_s]:
-            self.x += self.velocity * sin_a
-            self.y += -self.velocity * cos_a
-        if keys[pygame.K_w]:
-            self.x += -self.velocity * sin_a
-            self.y += self.velocity * cos_a
-        if keys[pygame.K_LEFT]:
-            self.angle -= 0.03
-        if keys[pygame.K_RIGHT]:
-            self.angle += 0.03
-    
+        if self.x <= 0 or self.x >= 1280:
+            self.angle = math.pi - self.angle
+        if self.y <= 0 or self.y >= 720:
+            self.angle = -self.angle
 
-        
+        self.x += self.velocity * math.cos(self.angle)
+        self.y += self.velocity * math.sin(self.angle)
